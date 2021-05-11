8a32aa32093e6b8da4a532b827e5be50f43ba177
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-05-11 16:22:29 +0400

    create(Sort - asc and desc): Create file and resolved task

---
+++ src/sort/1.ascDesc.js
@@ -0,0 +1,27 @@
+// Create a function that takes an array of numbers arr, a string str and return an array of numbers as per the following rules.
+
+// Asc function
+const sortedAsc = (array) => {
+  return array.sort((firstItem, secondItem) => {
+    return firstItem - secondItem
+  })
+}
+// Asc function
+const sortedDesc = (array) => {
+  return array.sort((firstItem, secondItem) => {
+    return secondItem - firstItem
+  })
+}
+
+
+const sortBy = (arr, position) => {
+  let sortedArr = []
+  if (position === 'Asc') sortedArr = sortedAsc(arr)
+  else if (position === 'Desc') sortedArr = sortedDesc(arr)
+
+  return sortedArr.length > 0 ? sortedArr : arr
+}
+
+console.log(sortBy([4, 3, 2, 1], "Asc" )) // [1, 2, 3, 4]
+console.log(sortBy([7, 8, 11, 66])) // [7, 8, 11, 66]
+console.log(sortBy([7, 8, 11, 66], "Desc")) // [66, 11, 8, 7]
