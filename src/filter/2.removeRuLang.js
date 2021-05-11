4d0fc889f6ef331cf2ff378eeb4bdd5043a12d78
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-05-11 18:22:27 +0400

    Create(Filter - Remove ru lang): Create file and resolved task

---
+++ src/filter/2.removeRuLang.js
@@ -0,0 +1,20 @@
+// Write a function which remove users with language equals to 'ru'.
+
+const users = [
+  {
+    username: "Yuri Gagarin",
+    lang: "ru",
+  },
+  {
+    username: "Nil Armstrong",
+    lang: "ENG",
+  },
+];
+
+function filterUsers(users) {
+  const removedRu = users.filter((el) => el.lang !== 'ru')
+
+  return removedRu
+}
+
+console.log(filterUsers(users)); // [{ username: "Nil Armstrong, lang: "ENG" }]
