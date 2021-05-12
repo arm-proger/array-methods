174ec899993f4fb7ec439eac4d05c32ae10b5987
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-05-12 13:58:11 +0400

    Create(Implements - forEach): Create file and resolved task

---
+++ src/implements/1.forEach.js
@@ -0,0 +1,22 @@
+// For each implement method
+const forEachImplement = (array, callback) => {
+  for(let index = 0; index < array.length; index++) {
+    callback(array[index], index, array)
+  }
+};
+
+
+// Example task
+const array1 = [1, 2, 3];
+
+let res = 1
+
+const getMulti = (element, idx, arr) => {
+  res *= element
+  console.log(idx, arr)
+  return res
+}
+
+forEachImplement(array1, getMulti);
+
+console.log(res)
