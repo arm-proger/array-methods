8525c21ecdac0282c3dd1ab90f7c036cff4af0a3
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-05-12 12:22:49 +0400

    Change(concate arrs): Change stucture

--- src/1.concateArrs.js
+++ src/1.concateArrs.js
@@ -5,20 +5,23 @@ const arr1 = [1, 2, 3, 4],
   arr2 = [5, 6, 7, 8, 9]
 
 // Concat Array
-const arr3 = [] // [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-function concatArrs(arr1, arr2, concatArr) {
-  const iterationCount = arr1.length + arr2.length - 1
+// function concatArrs(arr1, arr2, concatArr) {
+//   const iterationCount = arr1.length + arr2.length - 1
 
-  for (let i = 0; i <= iterationCount; i++) {
-    if (i < arr1.length) {
-      concatArr.push(arr1[i])
-    } else {
-      concatArr.push(arr2[i - arr1.length])
-    }
-  }
+//   for (let i = 0; i <= iterationCount; i++) {
+//     if (i < arr1.length) {
+//       concatArr.push(arr1[i])
+//     } else {
+//       concatArr.push(arr2[i - arr1.length])
+//     }
+//   }
 
-  return concatArr
+//   return concatArr
+// }
+
+const concatArrs = (...args) => {
+  return args.reduce((acc, el) => [...acc, ...el])
 }
 
-console.log(concatArrs(arr1, arr2, arr3))
+console.log(concatArrs(arr1, arr2))
