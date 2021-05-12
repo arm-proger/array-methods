4a4b069a02fca5b8c893f09199d269f761c591f7
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-05-12 17:05:12 +0400

    Create(Implement - map): Create file and resolved task

---
+++ src/implements/6.map.js
@@ -0,0 +1,21 @@
+// Find index implement method
+const mapImplement = (array, callback) => {
+  const mappedArr = []
+
+  for (let index = 0; index < array.length; index++) {
+    const pushItem = callback(array[index], index, array)
+
+    mappedArr.push(pushItem)
+  }
+
+  return mappedArr
+}
+
+// Example task
+let numbers = [4, 24, 7, 15]
+
+const isPrime = (element) => {
+  return (element *= 2)
+}
+
+console.log(mapImplement(numbers, isPrime))
