097635d4fcabd60d3b35d218310055d991c79614
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-05-12 17:01:59 +0400

    Create(Implement - find): Create file and resolved task

---
+++ src/implements/5.find.js
@@ -0,0 +1,18 @@
+// Find index implement method
+const findIndexImplement = (array, callback) => {
+  for (let index = 0; index < array.length; index++) {
+    if(callback(array[index], index, array)) {
+      return array[index]
+    }
+  }
+  return
+}
+
+// Example task
+let numbers = [4, 24, 7, 15]
+
+const isPrime = (element) => {
+  return element >= 100
+}
+
+console.log(findIndexImplement(numbers, isPrime))
